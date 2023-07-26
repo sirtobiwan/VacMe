4b4dc72c54f0cd25d3419c25ccf445cfa7bdb4aa
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-07-26 18:12:28 +0200

    setup frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Vaccine} from "./models/Vaccine.tsx";
 import axios from "axios";
 import Header from "./components/Header.tsx";
+import VaccineList from "./components/VaccineList.tsx";
 
 export default function App() {
 
     const [vaccines, setVaccines] = useState<Vaccine[]>([]);
 
+    useEffect(getAllVaccines, [])
     function getAllVaccines() {
         axios.get('/api/vaccine')
             .then(response => {
@@ -18,13 +20,12 @@ export default function App() {
             });
     }
 
-
-    if (!getAllVaccines)
-        return <h1> ... loading </h1>
-
     return (
         <>
             <Header/>
+            <main>
+                <VaccineList vaccines={vaccines}/>
+            </main>
 
         </>
     )
---
+++ frontend/src/components/VaccineCard.tsx
@@ -0,0 +1,30 @@
+import { Vaccines } from '../models/Vaccine';
+import { Card, CardContent, Typography } from '@mui/material';
+import './App.css';
+
+type Props = {
+    vaccine: Vaccines;
+};
+
+export default function VaccineCard({ vaccine }: Props) {
+    return (
+        <Card className="vaccine-card">
+            <CardContent>
+                <Typography variant="h6" className="vaccine-card-properties">
+                    {vaccine.disease}
+                </Typography>
+                <Typography variant="subtitle1" className="vaccine-card-properties">
+                    Vaccine Date: {vaccine.vaccineDate}
+                </Typography>
+                <Typography variant="body2" className="vaccine-card-properties">
+                    Due: {vaccine.due ? 'Yes' : 'No'}
+                </Typography>
+                {vaccine.due && (
+                    <Typography variant="body2" className="vaccine-card-properties">
+                        Due Date: {vaccine.dueDate}
+                    </Typography>
+                )}
+            </CardContent>
+        </Card>
+    );
+}
