27b619a32be4f83051ab538dd1a6ea2a829d0a53
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-08-02 15:27:38 +0200

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,11 @@ import {Vaccine, VaccineWithoutId} from "./models/Vaccine.tsx";
 import axios from "axios";
 import Header from "./components/Header.tsx";
 import VaccineList from "./components/VaccineList.tsx";
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 import Form from "./components/Form.tsx";
+import {IconButton} from "@mui/material";
+import {AddCircle} from "@mui/icons-material";
+
 
 export default function App() {
 
@@ -40,6 +43,11 @@ export default function App() {
                     <Form onSubmit={handleAddVaccine} />}
                 />
             </Routes>
+            <Link to={"/add"}>
+                <IconButton className={"add-button"} color="primary" aria-label="add vaccine">
+                    <AddCircle />
+                </IconButton>
+            </Link>
 
         </>
     )
