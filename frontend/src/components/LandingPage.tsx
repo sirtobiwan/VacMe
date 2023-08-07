ecb2f3238d663da3029a166167682e84edade027
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-08-07 11:59:48 +0200

    add landingpage due vaccines

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,7 +1,45 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Vaccine} from "../models/Vaccine.tsx";
+import { Alert } from '@mui/material';
+import '../App.css';
+import {LocalHospital} from "@mui/icons-material";
+
 export default function LandingPage() {
+
+    const [vaccines, setVaccines] = useState<Vaccine[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        axios.get('/api/vaccine')
+            .then(response => {
+                setVaccines(response.data);
+                setLoading(false);
+            })
+            .catch(function (error) {
+                console.error(error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <div>Loading...</div>
+
+    const getCurrentDate = () => {
+        const date = new Date();
+        return date.toISOString().split('T')[0];
+    }
+
+    const dueVaccines = vaccines.filter(vaccine => vaccine.dueDate && new Date(vaccine.dueDate) < new Date(getCurrentDate())).length;
+
     return (
-        <div>
+        <div className="landing-page">
+            <LocalHospital className="vaccine-icon" />
             <h1>Welcome to your Vaccine Tracker App</h1>
+            {dueVaccines > 0 ? (
+                <Alert severity="error">{dueVaccines} vaccination(s) due!</Alert>
+            ) : (
+                <Alert severity="success">No vaccinations are due!</Alert>
+            )}
         </div>
     );
 }
--- frontend/src/components/VaccineList.tsx
+++ frontend/src/components/VaccineList.tsx
@@ -1,5 +1,7 @@
 import { Vaccine} from '../models/Vaccine';
 import VaccineCard from './VaccineCard';
+import { Button } from '@mui/material';
+import { useState } from 'react';
 
 type Props = {
     vaccines: Vaccine[];
@@ -8,12 +10,47 @@ type Props = {
 };
 
 export default function VaccineList(props: Props) {
+    const [filter, setFilter] = useState('all');
+
     if (!props.vaccines)
         return <h1> ...loading </h1>
+
+    let filteredVaccines = props.vaccines;
+    if (filter === 'booster') {
+        filteredVaccines = props.vaccines.filter(vaccine => vaccine.due);
+    }
+
     return (
         <div>
             <h2>My Vaccines</h2>
-            {props.vaccines.map((vaccine) => (
+            <Button
+                variant="contained"
+                onClick={() => setFilter('all')}
+                sx={{
+                    width: '150px',
+                    backgroundColor: filter === 'all' ? '#3f51b5' : 'rgba(63, 81, 181, 0.3)',
+                    color: 'white',
+                    fontSize: '0.6rem',
+                    margin: '0.3rem 0.3rem 1rem 0.3rem'
+                }}
+            >
+                All Vaccines
+            </Button>
+            <Button
+                variant="contained"
+                onClick={() => setFilter('booster')}
+                sx={{
+                    width: '150px',
+                    backgroundColor: filter === 'booster' ? '#3f51b5' : 'rgba(63, 81, 181, 0.3)',
+                    color: 'white',
+                    fontSize: '0.6rem',
+                    margin: '0.3rem 0.3rem 1rem 0.3rem'
+                }}
+            >
+                Booster required
+            </Button>
+
+            {filteredVaccines.map((vaccine) => (
                 <VaccineCard
                     key={`${vaccine.disease}-${vaccine.vaccineDate}`}
                     vaccine={vaccine} onUpdate={props.onUpdate} onDelete={props.onDelete}
