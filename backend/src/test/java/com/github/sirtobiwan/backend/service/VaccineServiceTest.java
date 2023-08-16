8ffbe1ccae48af612c40e33fd0c33cbd0cee8b17
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-08-16 13:52:09 +0200

    fix tests

--- backend/src/test/java/com/github/sirtobiwan/backend/service/VaccineServiceTest.java
+++ backend/src/test/java/com/github/sirtobiwan/backend/service/VaccineServiceTest.java
@@ -4,6 +4,7 @@ import com.github.sirtobiwan.backend.exceptions.CountryNotFoundException;
 import com.github.sirtobiwan.backend.models.Vaccine;
 import com.github.sirtobiwan.backend.models.VaccineWithoutID;
 import com.github.sirtobiwan.backend.repo.VaccineRepo;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -26,8 +28,8 @@ class VaccineServiceTest {
     @Test
     void expectAllVaccines_whenAllVaccinesAreCalled() {
         //GIVEN
-        Vaccine vaccine1 = new Vaccine("123","Corona", "Biontech", "2" , LocalDate.now(),"Dr. Meier", true, LocalDate.now());
-        Vaccine vaccine2 = new Vaccine("456","Corona", "Johnson", "3" , LocalDate.now(),"Dr. Meier", true, LocalDate.now());
+        Vaccine vaccine1 = new Vaccine("123", "Corona", "Biontech", "2", LocalDate.now(), "Dr. Meier", true, LocalDate.now());
+        Vaccine vaccine2 = new Vaccine("456", "Corona", "Johnson", "3", LocalDate.now(), "Dr. Meier", true, LocalDate.now());
         List<Vaccine> expectedList = List.of(vaccine1, vaccine2);
         //WHEN
         when(vaccineRepo.findAll()).thenReturn(expectedList);
@@ -38,11 +40,11 @@ class VaccineServiceTest {
     }
 
     @Test
-    void expectVaccine_whenAddVaccine(){
+    void expectVaccine_whenAddVaccine() {
         //GIVEN
-        VaccineWithoutID vaccineWithoutID = new VaccineWithoutID("Corona", "Biontech", "2" , LocalDate.now(),"Dr. Meier", true, LocalDate.now());
+        VaccineWithoutID vaccineWithoutID = new VaccineWithoutID("Corona", "Biontech", "2", LocalDate.now(), "Dr. Meier", true, LocalDate.now());
         String randomId = "123";
-        Vaccine expectedVaccine = new Vaccine(randomId,"Corona", "Biontech", "2" , LocalDate.now(),"Dr. Meier", true, LocalDate.now());
+        Vaccine expectedVaccine = new Vaccine(randomId, "Corona", "Biontech", "2", LocalDate.now(), "Dr. Meier", true, LocalDate.now());
         //WHEN
         when(uuIdService.getRandomId()).thenReturn(randomId);
         when(vaccineRepo.insert(any(Vaccine.class))).thenReturn(expectedVaccine);
@@ -55,11 +57,11 @@ class VaccineServiceTest {
     }
 
     @Test
-    void expectUpdatedVaccine_whenUpdateVaccine(){
+    void expectUpdatedVaccine_whenUpdateVaccine() {
         //GIVEN
-        VaccineWithoutID vaccineWithoutID = new VaccineWithoutID("Corona", "Biontech", "2" , LocalDate.now(),"Dr. Meier", true, LocalDate.now());
+        VaccineWithoutID vaccineWithoutID = new VaccineWithoutID("Corona", "Biontech", "2", LocalDate.now(), "Dr. Meier", true, LocalDate.now());
         String randomId = "123";
-        Vaccine expectedVaccine = new Vaccine(randomId,"Corona", "Biontech", "2" , LocalDate.now(),"Dr. Meier", true, LocalDate.now());
+        Vaccine expectedVaccine = new Vaccine(randomId, "Corona", "Biontech", "2", LocalDate.now(), "Dr. Meier", true, LocalDate.now());
         //WHEN
         when(vaccineRepo.findById(randomId)).thenReturn(java.util.Optional.of(expectedVaccine));
         when(vaccineRepo.save(expectedVaccine)).thenReturn(expectedVaccine);
@@ -71,10 +73,10 @@ class VaccineServiceTest {
     }
 
     @Test
-    void deleteVaccine_whenDeleteVaccineByIdIsCalled(){
+    void deleteVaccine_whenDeleteVaccineByIdIsCalled() {
         //GIVEN
         String randomId = "123";
-        Vaccine vaccine = new Vaccine(randomId,"Corona", "Biontech", "2" , LocalDate.now(),"Dr. Meier", true, LocalDate.now());
+        Vaccine vaccine = new Vaccine(randomId, "Corona", "Biontech", "2", LocalDate.now(), "Dr. Meier", true, LocalDate.now());
         //WHEN
         when(vaccineRepo.findById(randomId)).thenReturn(java.util.Optional.of(vaccine));
         vaccineService.deleteVaccineById(randomId);
@@ -83,43 +85,46 @@ class VaccineServiceTest {
         verify(vaccineRepo).delete(vaccine);
     }
 
-        @Test
-        void expectVaccinationRecommendation_whenCountryIsValid() {
-            try (MockedStatic<Jsoup> jsoupMock = mockStatic(Jsoup.class)) {
-                //GIVEN
-                Document mockDoc = mock(Document.class);
-                Elements mockElements = mock(Elements.class);
-                String country = "Germany";
-                when(mockElements.eachText()).thenReturn(List.of("Recommendation 1", "Recommendation 2"));
-                when(mockDoc.select("div.col-xs-12 h2:contains(Impfempfehlung) + p + ul li")).thenReturn(mockElements);
-                jsoupMock.when(() -> Jsoup.connect("https://tropeninstitut.de/ihr-reiseziel/" + country).get()).thenReturn(mockDoc);
+    @Test
+    void expectVaccinationRecommendation_whenCountryIsValid() throws IOException {
+        // Given
+        String country = "Deutschland";
+        Document mockDoc = mock(Document.class);
+        Elements mockElements = mock(Elements.class);
+        Connection mockConnection = mock(Connection.class);
 
-                //WHEN
-                List<String> actualRecommendations = vaccineService.getVaccinationRecommendation(country);
+        when(mockElements.eachText()).thenReturn(List.of("Diphtherie", "Tetanus"));
+        when(mockDoc.select("div.col-xs-12 h2:contains(Impfempfehlung) + p + ul li")).thenReturn(mockElements);
+        when(mockConnection.get()).thenReturn(mockDoc);
 
-                //THEN
-                assertEquals(List.of("Recommendation 1", "Recommendation 2"), actualRecommendations);
-            }
+        try (MockedStatic<Jsoup> mockedJsoup = mockStatic(Jsoup.class)) {
+            mockedJsoup.when(() -> Jsoup.connect("https://tropeninstitut.de/ihr-reiseziel/" + country)).thenReturn(mockConnection);
+
+            // When
+            List<String> actualRecommendations = vaccineService.getVaccinationRecommendation(country);
+
+            // Then
+            assertEquals(List.of("Diphtherie", "Tetanus"), actualRecommendations);
         }
+    }
+    @Test
+    void expectCountryNotFoundException_whenCountryIsNotFound() throws IOException {
+        // Given
+        String country = "NonExistentCountry";
+        Connection mockConnection = mock(Connection.class);
+        Document mockDoc = mock(Document.class);
 
-        @Test
-        void expectCountryNotFoundException_whenCountryIsInvalid() {
-            try (MockedStatic<Jsoup> jsoupMock = mockStatic(Jsoup.class)) {
-                //GIVEN
-                Document mockDoc = mock(Document.class);
-                Elements mockElements = mock(Elements.class);
-                String invalidCountry = "InvalidCountry";
-                when(mockElements.eachText()).thenReturn(List.of());
-                when(mockDoc.toString()).thenReturn("404 - Seite nicht gefunden");
-                when(mockDoc.select("div.col-xs-12 h2:contains(Impfempfehlung) + p + ul li")).thenReturn(mockElements);
-                jsoupMock.when(() -> Jsoup.connect("https://tropeninstitut.de/ihr-reiseziel/" + invalidCountry).get()).thenReturn(mockDoc);
+        when(mockConnection.get()).thenReturn(mockDoc);
+        when(mockDoc.toString()).thenReturn("404 - Seite nicht gefunden");
 
-                //WHEN
-                Exception exception = assertThrows(CountryNotFoundException.class, () -> vaccineService.getVaccinationRecommendation(invalidCountry));
+        try (MockedStatic<Jsoup> mockedJsoup = mockStatic(Jsoup.class)) {
+            mockedJsoup.when(() -> Jsoup.connect("https://tropeninstitut.de/ihr-reiseziel/" + country)).thenReturn(mockConnection);
 
-                //THEN
-                assertTrue(exception.getMessage().contains("Fehler beim Abrufen der Impfempfehlung für das Land: " + invalidCountry));
-            }
+            // When & Then
+            assertThrows(CountryNotFoundException.class, () -> {
+                vaccineService.getVaccinationRecommendation(country);
+            });
         }
+    }
 
 }
